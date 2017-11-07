df7565d0685a71151ced5afc4d54458acf109e42
Chris Stuart <crstuart@gmail.com>
2017-11-06 20:31:02 -0500

    should be working fine now

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ const gulp = require('gulp'),
 	  sass = require('gulp-sass'),
 	  rename = require('gulp-rename'),
 	  minCss = require('gulp-clean-css'),
+	  changed = require('gulp-changed'),
+	  imagemin = require('gulp-imagemin'),
+	  del = require('del'),
+	  runSequence = require('run-sequence'),
+	  connect = require('gulp-connect'),
 	  maps = require('gulp-sourcemaps');
 
 
@@ -21,7 +26,18 @@ gulp.task("scripts", function(){
 	.pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task("compileSass", function() {
+gulp.task('images', function() {
+   var imgSrc = 'app/images/*.+(png|jpg)',
+   imgDst = 'dist/content';
+   
+   gulp.src(imgSrc)
+   .pipe(changed(imgDst))
+   .pipe(imagemin())
+   .pipe(gulp.dest(imgDst));
+});
+
+
+gulp.task("styles", function() {
   return gulp.src('app/sass/global.scss')
       .pipe(maps.init())
       .pipe(sass())
@@ -29,4 +45,29 @@ gulp.task("compileSass", function() {
       .pipe(rename('all.min.css'))
       .pipe(maps.write('./'))
       .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch('sass/**/*.scss', ['styles']);
+})
+
+gulp.task('clean', function(){
+	del('dist');
+
+})
+
+gulp.task('connect', function() {
+	connect.server();
+});
+
+gulp.task("build", ['clean'], function(callback) {
+	runSequence(['scripts', 'styles', 'images'], 
+	callback
+	)
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task("default", ["connect"], function() {
+	gulp.start('build');
 });
